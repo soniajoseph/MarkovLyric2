ee44c7e35d36e8c2134c46de8b92a0cb4ea4c95d
smjoseph <smjoseph@princeton.edu>
2019-08-14 04:22:32 -0400

    tweaks

--- app/routes.py
+++ app/routes.py
@@ -19,25 +19,26 @@ def lyrics():
 	try: 
 		# # Uncomment for RapGenius API
 		# # get artist
-		# artist = request.form['artist']
-		# genius = lyricsgenius.Genius("Y_izF-J78Qf8qn1gqLTWyKj95b_sJuQwj7f4smPQq7zB1qnMp3mJ71jpB2tBu0Bb")
-		# artist = genius.search_artist(artist, max_songs=2, sort="title")
+		artist = request.form['artist']
+		genius = lyricsgenius.Genius("Y_izF-J78Qf8qn1gqLTWyKj95b_sJuQwj7f4smPQq7zB1qnMp3mJ71jpB2tBu0Bb")
 
-		# # join lyrics into one document
-		# total_lyrics = []
-		# for song in artist.songs:
-		# 	total_lyrics.append(song.lyrics)
-		# total_lyrics = "".join(s for s in total_lyrics)
 
-		# # # test on bible
-		# f = open('rapgod.txt',"r")
-		# total_lyrics = f.read()
+		genius.verbose = True # Turn off status messages
+		genius.remove_section_headers = True # Remove section headers (e.g. [Chorus]) from lyrics when searching
+		genius.skip_non_songs = True # Include hits thought to be non-songs (e.g. track lists)
+		genius.excluded_terms = ["(Remix)", "(Live)"] # Exclude songs with these words in their title
 
-		# # test on user-entered lyrics
-		total_lyrics = request.form['artist']
+
+		artist = genius.search_artist(artist, max_songs=8, sort="popularity")
+
+		# join lyrics into one document
+		total_lyrics = []
+		for song in artist.songs:
+			total_lyrics.append(song.lyrics)
+		total_lyrics = "".join(s for s in total_lyrics)
 
 		model = MarkovModel()
-		lyrics = model.textGenerator(total_lyrics, 5, 4000)
+		lyrics = model.textGenerator(total_lyrics, 5, 1000)
 		lyrics = model.stringLyrics(lyrics)
 
 		# add line breaks
